26f5759154273adefcbb9fd87a15156f615f03ea
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-06 20:34:56 +0530

    Connect backend to the database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://social-media-heaven:social-mediaheaven123@realestate.zhwu98f.mongodb.net/';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {})
+    .then(() => app.listen(PORT, () => console.log(`Server listening on ${PORT}`)))
+    .catch(err => console.log(err.message));
+
+// mongoose.set('useFindAndModify', false);
