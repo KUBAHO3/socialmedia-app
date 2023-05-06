281f4eff524455a7b471edc3fc109f76395f633f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-07 00:18:09 +0530

    Add createPosts controllers logic

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -11,6 +11,16 @@ export const getPost = async (req, res) => {
     }
 };
 
-export const createPost = (req, res) => {
-    res.send("Create Post here"); 
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ error: error.message});
+    }
 };
