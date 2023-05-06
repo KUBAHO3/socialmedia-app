83fd995fff4dad90704b0433c77f8b469adab4b1
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-06 23:08:42 +0530

    Introduce controllers

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getPost } from '../controllers/posts.controllers.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Welcome Here');
-});
+router.get('/', getPost);
 
 export default router;
