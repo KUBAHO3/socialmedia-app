bc52ad881246a7319448b8fbb7f561ff336ce9a3
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-07 22:48:28 +0530

    Add getPosts action

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,11 +1,7 @@
 export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts;
-        case 'CREATE':
-            return posts;
-        case 'DELETE':
-            return posts;
+            return action.payload;
         default:
             break;
     }
